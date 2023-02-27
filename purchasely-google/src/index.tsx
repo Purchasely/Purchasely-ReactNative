2b87a5d70fa1e1d99bac4627f603183622814f99
Kevin Herembourg <kevin.herembourg@gmail.com>
2023-02-27 19:03:16 +0100

    fix: remove ios from purchasely-google

--- purchasely-google/src/index.tsx
+++ purchasely-google/src/index.tsx
@@ -2,7 +2,6 @@ import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-purchasely-google' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
