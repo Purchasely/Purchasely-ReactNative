ca11d20710ffdd73108dd8afaf77cd86bdc0ad34
Kevin Herembourg <kevin.herembourg@gmail.com>
2023-06-27 16:57:49 +0200

    chore: version 2.7.1

--- purchasely/src/index.ts
+++ purchasely/src/index.ts
@@ -1,6 +1,6 @@
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
-const purchaselyVersion = '2.7.0';
+const purchaselyVersion = '2.7.1';
 
 interface Constants {
   logLevelDebug: number;
