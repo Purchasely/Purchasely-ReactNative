c0113acf8940f29addd9a8a37e9369720f0ccd84
Kevin Herembourg <kevin.herembourg@gmail.com>
2024-01-17 16:46:23 +0100

    Version 4.2.3 (#89)
    * fix: android issue with r8 and StringConcat
    
    * iOS Event emitter (#90)
    
    * version 4.2.3
    
    ---------
    
    Co-authored-by: Huet Florian <florian.huet@epitech.eu>

--- purchasely/src/index.ts
+++ purchasely/src/index.ts
@@ -1,6 +1,6 @@
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
-const purchaselyVersion = '4.2.2';
+const purchaselyVersion = '4.2.3';
 
 interface Constants {
   logLevelDebug: number;
