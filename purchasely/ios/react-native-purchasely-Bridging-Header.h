5ff4e396068f906f85f69df49bdfbe1ad1e1ce79
Kevin Herembourg <kevin.herembourg@gmail.com>
2024-04-04 14:25:56 +0200

    Rename example bridging header to lib name

---
+++ purchasely/ios/react-native-purchasely-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  react-native-purchasely-Bridging-Header.h
+//  Bridge header for Purchasely SDK
+//
+//  Created by Kevin Herembourg on 14/03/2024.
+//
+
+#ifndef react_native_purchasely_Bridging_Header_h
+#define react_native_purchasely_Bridging_Header_h
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
+#import "React/RCTViewManager.h"
+#import "React/RCTComponent.h"
+
+#endif /* react_native_purchasely_Bridging_Header_h */
