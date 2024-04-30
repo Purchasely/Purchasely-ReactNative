bb7be358cdc49d186bfe5851d8725d32177c6ce3
Kevin Herembourg <kevin.herembourg@gmail.com>
2024-04-30 16:03:59 +0200

    Update PurchaselyViewManager.tsx
    Comment entire file

--- purchasely/src/PurchaselyViewManager.tsx
+++ purchasely/src/PurchaselyViewManager.tsx
@@ -1,2 +1,4 @@
-import {requireNativeComponent} from 'react-native';
-export const PurchaselyView = requireNativeComponent('PurchaselyView');
+// Useless file at the moment
+
+//import {requireNativeComponent} from 'react-native';
+//export const PurchaselyView = requireNativeComponent('PurchaselyView');
