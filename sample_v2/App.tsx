b9e910282b7f05bfa6364fb81cd90a798136204b
chouaibMo <chouaib@purchasely.com>
2024-09-03 11:30:03 +0200

    wip

--- sample_v2/App.tsx
+++ sample_v2/App.tsx
@@ -13,6 +13,8 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Button,
+  Alert,
   useColorScheme,
   View,
 } from 'react-native';
@@ -24,6 +26,131 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import Purchasely, {
+  LogLevels,
+  Attributes,
+  ProductResult,
+  RunningMode,
+  PLYPaywallAction,
+  PLYPresentationType,
+  type PurchaselyPresentation,
+  type PresentPresentationResult,
+} from 'purchasely';
+
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  async function setupPurchasely() {
+    var configured = false;
+    try {
+      // ApiKey and StoreKit1 attributes are mandatory
+      configured = await Purchasely.start({
+        apiKey: 'fcb39be4-2ba4-4db7-bde3-2a5a1e20745d',
+        storeKit1: false, // false to use StoreKit 2 and true to use StoreKit 1
+        logLevel: LogLevels.DEBUG, // to force log level for debug
+        userId: 'test-user', // if you know your user id
+        runningMode: RunningMode.FULL, // to set mode manually
+        androidStores: ['Google', 'Huawei'], // Google is already set by default
+      });
+    } catch (e) {
+      console.log('Purchasely SDK configuration error:', e);
+    }
+
+    if (!configured) {
+      console.error('Purchasely SDK initialization failed.');
+    } else {
+      console.info('Purchasely SDK initialized successfully.');
+    }
+
+    // logout the user
+    Purchasely.userLogout();
+
+    // check that the user is anonymous
+    await Purchasely.isAnonymous().then(isAnonymous => {
+      console.log('Is anonymous ? ' + isAnonymous);
+    });
+
+    // login the user
+    Purchasely.userLogin("test-user");
+
+    // check that the user is not anonymous anymore
+    await Purchasely.isAnonymous().then((isAnonymous) => {
+      console.log('User still anonymous ? ' + isAnonymous);
+    });
+  }
+
+  setupPurchasely();
+
+  return (
+    <SafeAreaView style={[backgroundStyle, { flex: 1 }]}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+
+        <Header />
+
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Display Presentation">
+            <Button
+               onPress={() => Alert.alert('Button pressed')}
+              title="Presentation"
+            />
+          </Section>
+
+          <Section title="Fetch Presentation">
+            <Button
+               onPress={() => Alert.alert('Button pressed')}
+              title="Presentation"
+            />
+          </Section>
+
+          <Section title="Display Nested View">
+            <Button
+               onPress={() => Alert.alert('Button pressed')}
+              title="Nested View"
+            />
+          </Section>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -54,65 +181,5 @@ function Section({children, title}: SectionProps): React.JSX.Element {
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
