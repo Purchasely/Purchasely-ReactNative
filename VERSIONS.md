4205fb93cb4bdb471b048b42e8b3764ddcc0e98d
Kevin Herembourg <kevin.herembourg@gmail.com>
2024-12-03 17:52:44 +0100

    version 4.5.4

--- VERSIONS.md
+++ VERSIONS.md
@@ -90,3 +90,4 @@ This file provides the underlying native SDK versions that the React Native SDK 
 | 4.5.1   | 4.5.1       | 4.5.2           |
 | 4.5.2   | 4.5.1       | 4.5.3           |
 | 4.5.3   | 4.5.2       | 4.5.5           |
+| 4.5.4   | 4.5.5       | 4.5.8           |
--- purchasely/src/index.ts
+++ purchasely/src/index.ts
@@ -2,7 +2,7 @@ import { NativeModules, NativeEventEmitter } from 'react-native';
 import { PLYPresentationViewBeta } from './PLYPresentationView';
 //import { PurchaselyView } from './PurchaselyViewManager';
 
-const purchaselyVersion = '4.5.3';
+const purchaselyVersion = '4.5.4';
 
 interface Constants {
   logLevelDebug: number;
