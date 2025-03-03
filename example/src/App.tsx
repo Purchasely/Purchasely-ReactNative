1debd34fe72ae6976afb5da3604e55f76ccf1c2f
Benjamin <benjamin.francois13@gmail.com>
2025-03-03 16:32:04 +0100

    feat(publish): add publish script

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { HomeScreen } from './Home.tsx'
