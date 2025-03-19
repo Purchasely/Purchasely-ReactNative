73986d01e47baa0a67f179f96ab7bbea0525ca7a
Kevin Herembourg <kevin.herembourg@gmail.com>
2025-03-19 17:52:56 +0100

    remove player folder

--- packages/player/src/index.ts
+++
@@ -1,5 +0,0 @@
-import ReactNativePurchaselyAndroidPlayer from './NativeReactNativePurchaselyAndroidPlayer';
-
-export function multiply(a: number, b: number): number {
-  return ReactNativePurchaselyAndroidPlayer.multiply(a, b);
-}
