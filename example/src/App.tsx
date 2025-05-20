bc987eba779c7775ea74385117586fa056a2539f
Kevin Herembourg <kevin.herembourg@gmail.com>
2025-05-20 14:17:55 +0200

    add test user id as sample

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -21,6 +21,7 @@ function App(): React.JSX.Element {
                 apiKey: 'fcb39be4-2ba4-4db7-bde3-2a5a1e20745d',
                 storeKit1: false, // false to use StoreKit 2 and true to use StoreKit 1
                 logLevel: LogLevels.DEBUG, // to force log level for debug
+                userId: 'test-user-id', // if you know your user id
                 runningMode: RunningMode.FULL, // to set mode manually
             })
         } catch (e) {
